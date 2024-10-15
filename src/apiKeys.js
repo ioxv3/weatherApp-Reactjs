f7b7929fa15d5bdd64ac176be46781d82630b351
Hamad <157201087+ioxv3@users.noreply.github.com>
2024-10-15 17:12:05 +0400

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{60edfcc98e36d10bc64c2d7dff9e50d8
+e}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
